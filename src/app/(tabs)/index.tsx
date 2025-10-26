90261e435106e3849f0fe130597b9b7937f9b5e1
Ulug2 <khaiul01@gettysburg.edu>
2025-10-25 20:21:22 -0400

    homescreen finished

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View, Image, StyleSheet, FlatList } from "react-native";
+import PostListitem from "../../components/PostListItem";
+import posts from "../../../assets/data/posts.json";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <FlatList
+        data={posts}
+        renderItem={({ item }) => <PostListitem post={item} />}
+      />
+    </View>
+  );
+}
